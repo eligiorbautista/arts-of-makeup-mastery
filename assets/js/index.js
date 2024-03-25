c0274a6af1890f5dfc6ff6345110258b194964d6
Eli Bautista <126312718+eligiorbautista@users.noreply.github.com>
2024-03-26 00:53:47 +0800

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,28 @@
-function valCheck() {
-    var menuClass = document.getElementById("menubar-icon");
-    if (document.getElementById('menubar').checked) {
-        menuClass.classList.remove("fa-xmark");
-    } else {
-        menuClass.classList.add("fa-xmark");
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+
+toggleBtn.addEventListener('click', () => {
+    /* This will run if the hamburger btn / toggle btn is clicked */
+    dropDownMenu.classList.toggle('open');
+
+    /* Check if dropdown menu is open and change icon to x if it is open */
+    const isOpen = dropDownMenu.classList.contains('open');
+    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+});
+
+window.addEventListener('resize', closeDropDownMenu);
+window.addEventListener('scroll', closeDropDownMenu);
+
+function closeDropDownMenu() {
+    if (window.innerWidth > 992 || window.scrollY > 0) {
+        if (dropDownMenu.classList.contains('open')) {
+            /* close dropdown menu */
+            dropDownMenu.classList.remove('open');
+            /* change icon to hamburger */
+            toggleBtnIcon.classList = 'fa-solid fa-bars';
+        }
     }
 }
 
-function closeNavBar() {
-    var menuClass = document.getElementById("menubar-icon");
-    document.getElementById('menubar').checked = false;
-    menuClass.classList.remove("fa-xmark");
-    menuClass.classList.add("fa-solid-fa-bars");
-}
 
-window.addEventListener('scroll', function () {
-    valCheck();
-    closeNavBar();
-});
